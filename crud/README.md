97fb28236e76f69b9abc0c360e7b9708f7d24e38
0xAckerMan <joelkores1@gmail.com>
2023-09-15 02:45:27 +0300

    Stage two Api webservice done

---
+++ crud/README.md
@@ -0,0 +1,10 @@
+### installation
+```
+go get github.com/gin-gonic/gin
+
+
+go get github.com/githubnemo/CompileDaemon
+
+go get -u gorm.io/gorm
+go get -u gorm.io/driver/postgres
+```
---
+++ crud/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/0xAckerMan/HNGx/crud/controllers"
+	"github.com/0xAckerMan/HNGx/crud/initializers"
+	"github.com/gin-gonic/gin"
+)
+
+func init(){
+	initializers.LoadEnvVariables()
+	initializers.ConnectToDatabase()
+}
+
+func main(){
+	r := gin.Default()
+	r.POST("/api", controllers.UsersCreate)
+	r.GET("/api", controllers.UsersIndex)
+	r.GET("/api/:id", controllers.UsersShow)
+	r.PUT("/api/:id", controllers.UsersUpdate)
+	r.DELETE("/api/:id", controllers.UsersDelete)
+
+	r.Run() // listen and serve on
+}
